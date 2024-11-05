e23fac47198b07f4175234dbfeadf8467d0786d1
Ima <okonekanem2004@yahoo.com>
2024-11-05 13:35:49 +0000

    replace button with toggle

--- popup.js
+++ popup.js
@@ -9,3 +9,23 @@ document.getElementById('focus-button').addEventListener('click', () => {
     }
 
 });
+
+let video;
+
+document.getElementById('focus-toggle').addEventListener('change', (event) => {
+    const output = document.getElementById("output");
+    const videoURL = "https://www.youtube.com/watch?v=6qXnPFytzU0"
+   
+
+    if (event.target.checked) {
+        output.innerText = "Focus Mode Activated";
+        video = window.open(videoURL, '_blank');
+    } else {
+        output.innerText = "Focus Mode deactivated";
+
+        if (video) {
+            video.close();
+            video = null;
+        }
+    }
+});
